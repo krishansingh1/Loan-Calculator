8ad0d809b4ff6820294c6bdc54307e4a57987734
Krishan Kumar <kkrishan7074@gmail.com>
2021-07-29 12:15:08 +0530

    Modified app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 document.getElementById('loan-form').addEventListener('submit', function(e) {
   
   //Hide Results
-    document.getElementById('results').style.display='none';
+    document.getElementById('results').style.
+    display='none';
 
   //Show Loader;
     document.getElementById('loading').style.display='block';
@@ -19,26 +20,36 @@ document.getElementById('loan-form').addEventListener('submit', function(e) {
      
     //console.log('Calculating...');
 
-    const amount= document.getElementById('amount');   
+    const amount= document.getElementById('amount');  
+
     const interest=document.getElementById('interest');
+
     const years= document.getElementById('years');
+
     const monthlyPayment= document.getElementById('monthly-payment');
+
     const totalPayment= document.getElementById('total-payment');
+
     const totalInterest= document.getElementById('total-interest');
 
      //Formulaes for Calculation
        const principle= parseFloat(amount.value);
+
        const calculatedInterest= parseFloat(interest.value) / 100 / 12;
+
        const calculatedPayments= parseFloat(years.value)*12;
 
        //Compute monthly payment
         const x= Math.pow(1 + calculatedInterest, calculatedPayments);
+
         const monthly= (principle*x*calculatedInterest)/(x-1);
 
         if(isFinite(monthly)){
 
             monthlyPayment.value= monthly.toFixed(2);
+
             totalPayment.value= (monthly * calculatedPayments).toFixed(2);
+
             totalInterest.value= ((monthly * calculatedPayments)-principle).toFixed(2);
 
          //Show Results
@@ -69,6 +80,7 @@ document.getElementById('loan-form').addEventListener('submit', function(e) {
 
      //Get Elements
      const card= document.querySelector('.card');
+
      const heading= document.querySelector('.heading');
 
      //Add Class
@@ -85,5 +97,6 @@ document.getElementById('loan-form').addEventListener('submit', function(e) {
   }
 
    function clearError() {
+     
      document.querySelector('.alert').remove();
    }
